c8e124a0c4c08038af9cd80dd7f848aca22e024c
mtrontzthedev <mtrontz@outlook.com>
2023-06-17 00:58:33 +0000

    fix: data routes

---
+++ app/models/job.server.ts
@@ -0,0 +1,142 @@
+import type { JobPost, JobApplication, User, Category } from "@prisma/client";
+
+import { prisma } from "~/db.server";
+// TODO: include strict validation to ensure only clients can make applications and employers can make job posts
+// import { getUserById, updateUserById } from '~/models/user.server';
+
+export type { JobPost, JobApplication } from "@prisma/client";
+
+/*
+type JobPost = {
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+    title: string;
+    description: string;
+    is_full_time: boolean;
+    start_date: Date;
+    end_date: Date | null;
+    responsibilities: string | null;
+    requirements: string | null;
+    salary_range_min: number;
+    salary_range_max: number;
+    userId: string;
+}
+
+type JobApplication = {
+    id: string;
+    userId: string;
+    jobId: string;
+    status: string;
+}
+
+type Category = {
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+    title: string;
+    description: string | null;
+}
+*/
+
+// TODO: might be unnecessary to have other get functions. 
+//filters can be applied to data returned from here. 
+//Also, the user.noteCount param is probably unnecessary since 
+//prisma has a count function that is filtered by user id.
+export async function getAllJobPosts() {   
+  return prisma.jobPost.findMany();
+};
+
+// TODO: maybe include a notification in each function to notify the user of results?
+
+export async function getAllJobApplications() {   
+    return prisma.jobApplication.findMany();
+  };
+
+  export async function categoryExists(title: Category['title']) {
+    return prisma.category.findFirst({where: {title}})
+  }
+
+  export async function createJobPost(userId: User["id"], job: Omit<JobPost, 'id' | 'createdAt' | 'updatedAt' | 'userId'>, categories: Pick<Category, 'id' | 'title'>[] = []) {
+    
+    const categoryConnectOrCreate = categories.map((cat) => ({
+      where: { id: cat.id },
+      create: { title: cat.title },
+    }));
+  /*
+  {
+            where: {id: categories[0].id},
+            create: {title: categories[0].title}
+          }
+          */
+    return prisma.jobPost.create({
+      data: {
+        ...job,
+        userId,
+        categories: {
+          connectOrCreate: categoryConnectOrCreate
+        },
+      },
+    });
+  }
+  
+  /*
+  export async function createJobPost(userId: User["id"], job: Omit<JobPost, 'id' | 'createdAt' | 'updatedAt' | 'userId'>, categories: Category['title'][]) {
+    const check = async () => categories.forEach(async (cat) => {
+        const exists = await categoryExists(cat);
+        if (!exists) {
+            return await prisma.category.create({data: {title: cat}});
+        };
+    });
+    await check().then(() => prisma.jobPost.create({
+        data: {
+          ...job,
+          userId,
+          categories: {
+            connect: categories.map((cat) => {return {connect: {title: cat}}})
+          },
+        },
+      });)
+    // const categoryConnectOrCreate = categories.map((title) => ({
+      // where: { title }
+    // }));
+  
+    
+  }
+*/
+
+export async function createJobApplication(userId: User["id"], jobId: JobPost['id']) {
+    
+    return prisma.jobApplication.create({
+      data: {
+        status: 'submitted',
+        userId,
+        jobId,
+      },
+    });
+  };
+
+  export async function updateJobApplicationStatus(id: JobApplication["id"], status: JobApplication['status']) {
+    // TODO: add validation to ensure only specific options are set for status
+    return prisma.jobApplication.update({
+      where: { id },
+      data: { status }
+    });
+  };
+
+  export async function createCategory(data: Pick<Category, 'title' | 'description'>) {
+    return prisma.category.create({ data });
+  };
+
+export async function deleteJobPost(id: JobPost["id"]) {
+  return prisma.jobPost.delete({ where: { id } });
+};
+
+export async function deleteJobApplication(id: JobApplication["id"]) {
+    return prisma.jobApplication.delete({ where: { id } });
+  };
+
+  export async function deleteCategory(id: Category["id"]) {
+    return prisma.category.delete({ where: { id } });
+  };
+
--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -1,9 +1,13 @@
-import type { Password, User } from "@prisma/client";
+import type { User } from "@prisma/client";
 import bcrypt from "bcryptjs";
 
 import { prisma } from "~/db.server";
 
 export type { User } from "@prisma/client";
+
+export async function updateUserById(id: User["id"], updates: Partial<User>) {
+  return prisma.user.update({where: {id}, data: updates});
+}
 
 export async function getUserById(id: User["id"]) {
   return prisma.user.findUnique({ where: { id } });
@@ -13,50 +17,47 @@ export async function getUserByEmail(email: User["email"]) {
   return prisma.user.findUnique({ where: { email } });
 }
 
-export async function createUser(email: User["email"], password: string) {
+export async function createUser(email: User["email"], password: User["password_hash"]) {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   return prisma.user.create({
     data: {
       email,
-      password: {
-        create: {
-          hash: hashedPassword,
-        },
-      },
+      password_hash: hashedPassword,
     },
   });
 }
+
+export async function deleteUserById(id: User["id"]) {
+  return prisma.user.delete({ where: { id } });
+};
 
 export async function deleteUserByEmail(email: User["email"]) {
   return prisma.user.delete({ where: { email } });
-}
+};
 
 export async function verifyLogin(
   email: User["email"],
-  password: Password["hash"]
+  password: User["password_hash"]
 ) {
   const userWithPassword = await prisma.user.findUnique({
-    where: { email },
-    include: {
-      password: true,
-    },
+    where: { email }
   });
 
-  if (!userWithPassword || !userWithPassword.password) {
+  if (!userWithPassword || !userWithPassword.password_hash) {
     return null;
   }
 
   const isValid = await bcrypt.compare(
     password,
-    userWithPassword.password.hash
+    userWithPassword.password_hash
   );
 
   if (!isValid) {
     return null;
   }
 
-  const { password: _password, ...userWithoutPassword } = userWithPassword;
+  const { password_hash: _password, ...userWithoutPassword } = userWithPassword;
 
   return userWithoutPassword;
-}
+};
