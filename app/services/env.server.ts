717a948ea500cd77b06a578a49eaff4172707629
Matthew Trontz <mtrontz@outlook.com>
2023-09-02 06:54:46 -0400

    Update env.server.ts

--- app/services/env.server.ts
+++ app/services/env.server.ts
@@ -16,6 +16,21 @@ declare global {
   interface Window {
     ENV: ENV
   }
+};
+
+/**
+ * @returns domain URL (without a ending slash, like: https://kentcdodds.com)
+ */
+function getDomainUrl(request: Request) {
+  const host =
+    request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
+  if (!host) {
+    throw new Error('Could not determine domain URL.')
+  }
+  const protocol = host.includes('localhost') ? 'http' : 'https'
+  return `${protocol}://${host}`
 }
 
-export {getEnv}
+export { getEnv, getDomainUrl };
+
+
